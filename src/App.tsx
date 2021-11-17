c96cba0db926778b6814b199a3e4225f8fa4615a
rafaelibiapina <rafaelibiapina@sefaz.pi.gov.br>
2021-11-17 13:10:33 -0300

    creating Modal NewTransaction

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
+import { useState } from 'react';
+import Modal from 'react-modal'
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 
+Modal.setAppElement('#root')
+
 export function App() {
+
+
+  const [ isNewTransactionModalOpen,setINewTransactionModalOpen] = useState(false);
+
+
+  function handleOpenNewTransactionModal(){
+
+    setINewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal(){
+    setINewTransactionModalOpen(false);
+  }
+
   return (
 
     <div className="App">
       <GlobalStyle/>
-      <Header/>
+      <Header onOpenNewTransactionModal={ handleOpenNewTransactionModal } />
       <Dashboard/>
+      <Modal 
+        isOpen={isNewTransactionModalOpen} 
+        onRequestClose={handleCloseNewTransactionModal} >
+          <h2>Cadastrar transação</h2>
+        </Modal>
       <h1>Hello World</h1>
       
     </div>
