8500ae9d4eff85bf389ac212b2b438d26620f161
rafaelibiapina <rafaelibiapina@sefaz.pi.gov.br>
2021-11-18 13:33:38 -0300

    Styling NewTransactionType Button

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,9 @@
 import Modal from 'react-modal';
-import closeImg from '../../assets/close.svg'
-import { Container } from './styles';
+import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+
+import { Container,TransactionTypeContainer } from './styles';
 
 
 interface NewTransactionModalProps{
@@ -31,6 +34,20 @@ export function NewTransactionModal ( { isOpen , onRequestClose} : NewTransactio
                 <input type="text"  placeholder="Titulo"/>
 
                 <input type="number" placeholder="Valor" />
+
+                <TransactionTypeContainer>
+
+                    <button type="button">
+                        <img src={incomeImg} alt="Entrada" />
+                        <span>Entrada</span>
+                    </button>
+
+                    <button type="button">
+                        <img src={outcomeImg} alt="Saída" />
+                        <span>Saída</span>
+                    </button>
+
+                </TransactionTypeContainer>
 
                 <input type="text" placeholder="Categoria" />
 
