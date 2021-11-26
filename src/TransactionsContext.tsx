57d9b7dc8dfe508f4424d03df0cd57191743938f
rafaelibiapina <rafaelibiapina@sefaz.pi.gov.br>
2021-11-26 13:30:24 -0300

    Listing a new transaction created

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -10,13 +10,30 @@ interface Transaction {
     createdAt:string;
   };
 
+    type TransactionInput = Omit<Transaction,'id' | 'createdAt'>;
+  
+//   type TransactionInput = Pick<Transaction, 'title' | 'amount' | 'type' | 'category'>;
+// interface TransactionInput {
+//     title:string;
+//     amount:number;
+//     type:string;
+//     category:string;
+// }
+
 interface TransactionsProviderProps {
 
     children:ReactNode;
 }
 
 
-export const TransactionsContext = createContext<Transaction[]>([]);
+interface TransactionsContextData {
+    transactions : Transaction[];
+    createTransaction : (transaction : TransactionInput ) => Promise<void>;
+}
+
+export const TransactionsContext = createContext<TransactionsContextData>(
+    {} as TransactionsContextData
+);
 
 export function TransactionsProvider( { children }:TransactionsProviderProps) {
 
@@ -25,10 +42,26 @@ export function TransactionsProvider( { children }:TransactionsProviderProps) {
     useEffect(()=>{
       api.get('transactions')
       .then(response => setTransactions( response.data.transactions ))
-    },[])
+    },[]);
+    
+    async function createTransaction(transactionInput : TransactionInput){
+
+        const response =await api.post('/transactions',{
+            ...transactionInput,
+            createdAt: new Date(),
+        });
+        const { transaction } = response.data;
+
+        setTransactions([
+            ...transactions,
+            transaction
+        ])
+        
+    }
+
 
     return (
-        <TransactionsContext.Provider value={transactions}>
+        <TransactionsContext.Provider value={{transactions,createTransaction}}>
             {children}
         </TransactionsContext.Provider>
     )
