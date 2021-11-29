5f591822597982aff18c511df7d6bf98d4fbe541
RafaelMatos <rafael-matos@outlook.com>
2021-11-28 23:35:03 -0300

    Hooks for transactions added

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,5 +1,5 @@
-import { createContext, useEffect, useState, ReactNode } from 'react';
-import { api } from './services/api';
+import { createContext, useEffect, useState, ReactNode, useContext } from 'react';
+import { api } from '../services/api';
 
 interface Transaction {
     id:number;
@@ -31,7 +31,7 @@ interface TransactionsContextData {
     createTransaction : (transaction : TransactionInput ) => Promise<void>;
 }
 
-export const TransactionsContext = createContext<TransactionsContextData>(
+const TransactionsContext = createContext<TransactionsContextData>(
     {} as TransactionsContextData
 );
 
@@ -64,5 +64,10 @@ export function TransactionsProvider( { children }:TransactionsProviderProps) {
         <TransactionsContext.Provider value={{transactions,createTransaction}}>
             {children}
         </TransactionsContext.Provider>
-    )
+    );
+}
+
+export function useTransactions() {
+    const context = useContext(TransactionsContext);
+    return context;
 }
